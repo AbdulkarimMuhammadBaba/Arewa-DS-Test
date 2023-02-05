ffb5a83a131e9014038bc88ba887a06f1082de65
Abdulkarim Muhammad Baba <123999820+AbdulkarimMuhammadBaba@users.noreply.github.com>
2023-02-05 09:18:55 +0100

    Add files via upload

---
+++ Exercise Level 2 Repeating question 1-4.py
@@ -0,0 +1,24 @@
+# 1: cheking version of python
+python --version
+# 2: doing operations
+print(8+8)
+print(9-5)
+print(4*5)
+print(5%2)
+print(5/2)
+print(2**3)
+print(5//2)
+# 3: writing strings
+print("Abdulkarim", "Muhammad")
+print("Baba")
+print("Nigeria")
+print("I am enjoying 30days of python")
+#4: Checking datatype
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Abdulkarim"))
+print(type("Baba"))
+print(type("Nigeria"))
